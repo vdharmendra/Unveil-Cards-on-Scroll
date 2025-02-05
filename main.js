8c816e8f2de11ef49611f595d82001bc02e0cd6d
vdharmendra <dhiru860.com>
2025-02-05 19:50:33 +0530

    UPLOADING FILES

---
+++ main.js
@@ -0,0 +1,39 @@
+function Utils() {}
+Utils.prototype = {
+    constructor: Utils,
+    isElementInView: function (element, fullyInView) {
+        var pageTop = $(window).scrollTop();
+        var pageBottom = pageTop + $(window).height();
+        var elementTop = $(element).offset().top;
+        var elementBottom = elementTop + $(element).height();
+
+        if (fullyInView === true) {
+            return ((pageTop < elementTop) && (pageBottom > elementBottom));
+        } else {
+            return ((elementTop <= pageBottom) && (elementBottom >= pageTop));
+        }
+    }
+};
+
+function addFadeIn(repeat) {
+    var classToFadeIn = ".will-fadeIn";
+    $(classToFadeIn).each(function (index) {
+    var isElementInView = Utils.isElementInView($(this), false);
+    if (isElementInView) {
+        if (!$(this).hasClass("fadeInRight") && !$(this).hasClass("fadeInLeft")) {
+        if (index % 2 == 0) $(this).addClass("fadeInRight");
+        else $(this).addClass("fadeInLeft");
+        }
+    } else if (repeat) {
+        $(this).removeClass("fadeInRight");
+        $(this).removeClass("fadeInLeft");
+    }
+    });
+}
+
+var Utils = new Utils();
+$(window).on('load', addFadeIn());
+
+$(window).scroll(function() {
+    addFadeIn(true);
+});
